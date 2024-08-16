f6711f69fd412280241fafff4c9160b6bcf1766f
Piraya Sutthiparinyanon <a677248@AR8040677248N1.local>
2024-08-16 11:47:06 +0700

    add README file

--- main.go
+++ main.go
@@ -79,7 +79,7 @@ type Skill struct {
 	Key         string   `json:"key"`
 	Name        string   `json:"name"`
 	Description string   `json:"description"`
-	Logo        string   `json:"logo`
+	Logo        string   `json:"logo"`
 	Tags        []string `json:"tags"`
 }
 
