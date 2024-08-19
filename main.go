fe4716593d2b2fc5a28dc38b904d13c2169a5c7b
Cartoonydesu <toon.pi45@gmail.com>
2024-08-19 10:56:24 +0700

    clean code

--- main.go
+++ main.go
@@ -19,11 +19,8 @@ import (
 )
 
 func main() {
-	// fmt.Println("Hello world.")
-	// logs := logger.New()
 	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
-
 	connStr := "postgres://postgres:1234@127.0.0.1:5432/app?sslmode=disable"
 	var err error
 	db, err := sql.Open("postgres", connStr)
@@ -36,15 +33,12 @@ func main() {
 	}
 	h := &handler{db: db}
 	router := gin.Default()
-
 	setRouter(router, h)
-
 	srv := http.Server{
 		Addr:        ":" + os.Getenv("PORT"),
 		Handler:     router,
 		ReadTimeout: 3 * time.Second,
 	}
-
 	go func() {
 		<-ctx.Done()
 		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
@@ -54,15 +48,12 @@ func main() {
 			log.Fatal(err)
 		}
 	}()
-
 	if err := srv.ListenAndServe(); err != nil {
 		if !errors.Is(err, http.ErrServerClosed) {
 			log.Fatal(err)
 		}
 	}
-
 	slog.Info("bye")
-
 }
 
 type handler struct {
@@ -73,9 +64,6 @@ func getPing(context *gin.Context) {
 	context.JSON(http.StatusOK, gin.H{"message": "pong"})
 }
 
-//	func newRouter() {
-//		router := gin.Default()
-//	}
 type Skill struct {
 	Key         string   `json:"key"`
 	Name        string   `json:"name"`
@@ -278,7 +266,6 @@ func (h *handler) updateSkillTags(context *gin.Context) {
 		return
 	}
 	h.getSkillByKey(paramkey, context)
-
 }
 
 func (h *handler) deleteSkill(context *gin.Context) {
